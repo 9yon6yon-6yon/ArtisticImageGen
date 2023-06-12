957469f0eed7753444f5e06be2969ee30cc65c11
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-06-12 13:03:21 +0600

    instructions added

---
+++ README.md
@@ -0,0 +1,51 @@
+# <center>Artistic Image Generation</center>
+
+![Banner](/banner.png)
+
+This repository contains a script for generating unique artworks by combining different body parts.
+
+## Folder Arrangement
+
+The folders should be arranged as follows:
+
+1. backgrounds/
+2. body/
+3. hair/
+4. hat/
+5. glasses/
+6. beard/
+7. smoke/
+8. hoodie/
+9. breclet/
+10. socks/
+11. shoe/
+12. jacket/
+
+Each folder should contain PNG files representing the respective body part. Make sure the PNG files have `transparent` backgrounds.
+
+## Prerequisites
+
+- Python 3.x
+- PIL (Python Imaging Library)
+
+## How to Run
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/9yon6yon-6yon/ArtisticImageGen.git
+cd ArtisticImageGen
+pip install -r requirements.txt
+```
+Modify the folders dictionary in the generate_artwork.py file to specify the paths to your folders containing the PNG files and the path to the output file.
+
+Run the script:
+
+```bash
+python generate_artwork.py <num_artworks>
+```
+Replace `<num_artworks>` with the desired number of unique artworks to generate.
+The generated artworks will be saved in the `output/` folder.
+
+<hr>
+Feel free to customize and modify the content according to your specific requirements.
---
+++ generate_artwork.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import random
+import shutil
+from PIL import Image
+
+# Set the paths to your folders containing the PNG files
+folders = {
+    'backgrounds': '/grbackground',
+    'body': '/body',
+    'hair': '/hair',
+    'hat': '/hat',
+    'glasses': '/sunglass',
+    'beard': '/beard',
+    'smoke': '/smoke',
+    'hoodie': '/hoodie',
+    'breclet': '/chain',
+    'jacket': '/jacket',
+    'socks': '/socks',
+    'shoe': '/shoes'
+  
+     
+}
+
+# Set the output folder path
+output_folder = '/output/'
+
+# Get the number of unique artworks from the command-line argument
+num_artworks = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Create a list of available PNG files for each body part
+png_files = {}
+for folder_name, folder_path in folders.items():
+    png_files[folder_name] = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.png')]
+
+# Generate unique artwork
+generated_artworks = set()
+for _ in range(num_artworks):
+    # Generate a random combination of PNG files
+    artwork = {}
+    for folder_name, files in png_files.items():
+        artwork[folder_name] = random.choice(files)
+
+    # Create a unique identifier for the artwork based on the chosen files
+    artwork_id = '-'.join([os.path.basename(file).split('.')[0] for file in artwork.values()])
+
+    # Check if the artwork has already been generated
+    if artwork_id not in generated_artworks:
+        generated_artworks.add(artwork_id)
+
+        # Create a new image with a transparent background
+        canvas = Image.new('RGBA', (1080, 1080), (0, 0, 0, 0))
+
+        # Paste each body part onto the canvas
+        for file_path in artwork.values():
+            part_image = Image.open(file_path).convert('RGBA')
+            canvas.paste(part_image, (0, 0), mask=part_image)
+
+        # Save the merged image
+        image_path = os.path.join(output_folder, f"{artwork_id}.png")
+        canvas.save(image_path)
+
+print(f"{num_artworks} unique artworks generated in the '{output_folder}' folder.")
